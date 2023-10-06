abea8010795c51e6743a8893e202b065ccefac68
Sabaheer <sabaheer34@gmail.com>
2023-10-06 21:42:55 +0500

    Commit

---
+++ app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import cohere
+import numpy as np
+import faiss
+
+st.set_page_config(page_title='CoDoctor', page_icon = "images/favicon.png", initial_sidebar_state = 'auto')
+
+
+st.image("images/logo.png", use_column_width="auto", width=250)
+
+
+
+# Paste your API key here. Remember to not share publicly
+api_key = "lhNxyU2hx99v6EvkwTqi9hykwpTgaL7cHKWSYd2M"
+
+class SemanticSearch():
+
+    def __init__(self, path, key):
+        self.co = cohere.Client(key)
+        self.embeddings = np.load(path,allow_pickle=True)
+        self.index = faiss.IndexFlatIP(self.embeddings['embeddings'].shape[-1])  # inner product index
+        f32_embeddings = self.embeddings['embeddings'].astype(np.float32)  # faiss only supports f32
+
+        # normalise embeddings, so that the inner product is the cosine similarity
+        normal_f32_embeddings = f32_embeddings / np.linalg.norm(f32_embeddings, ord=2, axis=-1, keepdims=True)
+        self.index.add(normal_f32_embeddings)
+        
+    def query(self, query, num_query=10):
+        q_embedding = self.co.embed([query], model='multilingual-22-12').embeddings
+        q = np.array(q_embedding, dtype=np.float32)
+
+        # normalise the query too
+        normal_q = q / np.linalg.norm(q, ord=2, axis=-1, keepdims=True)
+        distances, ind = map(np.squeeze, self.index.search(normal_q, num_query))
+
+        results = []
+        for i, dist in zip(ind, distances):
+            result = {
+                "question": self.embeddings['questions'][i],
+                "url": self.embeddings['url'][i],
+                "answer": self.embeddings['answer'][i],
+            }
+            results.append(result)
+        return results
+
+
+
+st.title("Welcome to CoDoctor, Your Personal Multi-Lingual Medical Help Centre with Answers to all your Queries!")
+
+search = SemanticSearch("model/semantic_search.npz", api_key)
+# option = st.selectbox(
+#     'Query Language?',
+#     ('English','German', 'Chinease', 'French','Urdu')) # for drop down menu
+
+# if 'query' not in st.session_state:
+#     st.session_state.query = "COVID"
+# else:
+#     st.session_state.query = st.session_state.query
+query = st.text_input("Enter your query", "COVID")#st.session_state.query)
+# st.session_state.query = query
+x = st.slider("Select an number of queries", 2, 25, 3)
+# from translate import Translator
+
+
+# print 
+
+# translator = Translator(to_lang=option.lower())
+# translation = translator.translate(query  )
+results = search.query(query, int(x))
+
+st.title("**Results:**")
+
+for r in results:
+
+    
+    # Questions
+    st.subheader(f"**{r['question']}**")
+    # Answers
+    st.write(r["answer"])
+    #here u can add designs to the output
+    #link 
+    st.write(f"**{r['url']}**")
+    st.write("-"*80)
+# st.write(f"{results}")
+
+
+
+
+
+
